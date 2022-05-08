3c99274b5a67b545767066497130b60069ac4663
yoonsu Kim <iamkys0417@gmail.com>
2022-05-09 00:23:39 +0900

    시작화면 ui

---
+++ Assets/Scripts/NPC/AgencyText.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AgencyText : MonoBehaviour
+{
+    public float time;
+
+    public Text text;
+
+    int num;
+
+    public GameObject button1;
+
+    // Update is called once per frame
+    void Update()
+    {
+        time += Time.deltaTime;
+
+        num = (int)time;
+        switch(num)
+        {
+            case 1:
+                {
+                    text.text = " 첫 번째 대사.. ~~~~~~ ";
+                    break;
+                }
+            case 5:
+                {
+                    text.text = " 두 번째 대사.. !!!!!!!";
+                    break;
+                }
+            case 9:
+                {
+                    text.text = " 세 번째 대사.. ********";
+                    break;
+                }
+            case 13:
+                {
+                    text.text = " 네 번째 대사.. ^^^^^^^^";
+                    button1.SetActive(true);
+                    break;
+                }
+        }
+
+    }
+}
---
+++ Assets/Scripts/SceneMove/GameMng.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameMng : MonoBehaviour
+{
+    public GameObject MenuSet;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Cancel"))
+        {
+            if (MenuSet.activeSelf)
+            {
+                MenuSet.SetActive(false);
+            }
+            else
+            {
+                MenuSet.SetActive(true);
+            }
+        }
+    }
+
+    public void GameExit()
+    {
+        Application.Quit();
+    }
+}
